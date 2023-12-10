d27200a401fdf4d05ea0752bbd35931fc9b8d939
Yves Tscherry <info@feerglas.com>
2023-12-10 18:37:29 +0100

    comment supabase comment

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,16 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
-);
+// export const supabase = createClient(
+//   import.meta.env.SUPABASE_URL,
+//   import.meta.env.SUPABASE_ANON_KEY,
+// );
 
-export const tableNames = {
-  fruits: 'fruits',
-  vegetables: 'vegetables',
-  spices: 'spices',
-};
+// export const tableNames = {
+//   fruits: 'fruits',
+//   vegetables: 'vegetables',
+//   spices: 'spices',
+//   herbs: 'herbs',
+//   cereals: '',
+//   liquids: '',
+//   nuts: '',
+// };
