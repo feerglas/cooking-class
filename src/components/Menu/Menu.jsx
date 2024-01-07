00f6d4f5d37dbd42c53d9f37777849ec2146fa7a
Yves Tscherry <info@feerglas.com>
2024-01-07 14:14:12 +0100

    add logic to add menuCourses on click

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,22 +1,57 @@
+import {
+  createSignal,
+  For,
+} from 'solid-js';
 import AddBar from '../AddBar/AddBar';
 import MenuCourse from '../MenuCourse/MenuCourse';
 import styles from './Menu.module.scss';
 
 export default function Menu(props) {
+  const [
+    courses,
+    setCourses,
+  ] = createSignal([
+    {
+      ...props.menuCourse,
+      id: 'course-1',
+    },
+  ]);
+
+  const addCourseAtIndex = (index) => {
+    const addedCourse = [
+      ...courses()
+        .slice(0, index),
+      {
+        ...props.menuCourse,
+        id: `course-${courses().length + 1}`,
+      },
+      ...courses()
+        .slice(index),
+    ];
+
+    setCourses(addedCourse);
+  };
+
   return (
     <div class={styles['menu']}>
       <AddBar click={() => {
-        console.log('click');
+        addCourseAtIndex(0);
       }} />
 
-      <MenuCourse
-        {...props.menuCourse}
-        index='2'
-      />
+      <For each={courses()}>
+        {(course, index) => (
+          <div>
+            <MenuCourse
+              {...course}
+              index={index() + 1}
+            />
+            <AddBar click={() => {
+              addCourseAtIndex(index() + 1);
+            }} />
+          </div>
+        )}
+      </For>
 
-      <AddBar click={() => {
-        console.log('click');
-      }} />
     </div>
   );
 }
