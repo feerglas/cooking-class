571043ccc7b6e4334fb65d7fe2de9792d4158580
Yves Tscherry <info@feerglas.com>
2024-01-07 16:03:19 +0100

    add components data for whole menu component

--- src/data/componentsData.js
+++ src/data/componentsData.js
@@ -1,52 +1,85 @@
-export default {
-  ingredientsAutocompleteInputDropdown: {
-    button: {
-      click: false,
-      color: 'gray',
-      iconAfter: 'arrowDown',
-      iconBefore: false,
-      label: 'Alle',
-      noRightBorderRadius: true,
-      size: 'l',
+const ingredientsAutocompleteInputDropdown = {
+  button: {
+    click: false,
+    color: 'gray',
+    iconAfter: 'arrowDown',
+    iconBefore: false,
+    label: 'Alle',
+    noRightBorderRadius: true,
+    size: 'l',
+  },
+  items: [
+    {
+      radio: {
+        checked: true,
+        id: 'ingrdients-selector-all',
+        label: 'Alle',
+        name: 'ingrdients-selector',
+        value: 'all',
+      },
     },
-    items: [
-      {
-        radio: {
-          checked: true,
-          id: 'ingrdients-selector-all',
-          label: 'Alle',
-          name: 'ingrdients-selector',
-          value: 'all',
-        },
+    {
+      radio: {
+        checked: false,
+        id: 'ingrdients-selector-vegetables',
+        label: 'Vegetables',
+        name: 'ingrdients-selector',
+        value: 'vegetables',
       },
-      {
-        radio: {
-          checked: false,
-          id: 'ingrdients-selector-vegetables',
-          label: 'Vegetables',
-          name: 'ingrdients-selector',
-          value: 'vegetables',
-        },
+    },
+    {
+      radio: {
+        checked: false,
+        id: 'ingrdients-selector-fruits',
+        label: 'Fruits',
+        name: 'ingrdients-selector',
+        value: 'fruits',
       },
-      {
-        radio: {
-          checked: false,
-          id: 'ingrdients-selector-fruits',
-          label: 'Fruits',
-          name: 'ingrdients-selector',
-          value: 'fruits',
-        },
+    },
+    {
+      radio: {
+        checked: false,
+        id: 'ingrdients-selector-herbs',
+        label: 'Herbs',
+        name: 'ingrdients-selector',
+        value: 'herbs',
       },
-      {
-        radio: {
-          checked: false,
-          id: 'ingrdients-selector-herbs',
-          label: 'Herbs',
-          name: 'ingrdients-selector',
-          value: 'herbs',
+    },
+  ],
+  radios: true,
+};
+
+export default {
+  menuCourse: {
+    button: {
+      color: 'red',
+      iconAfter: 'trash',
+      iconBefore: false,
+      label: 'Löschen',
+      outline: true,
+      size: 's',
+    },
+    heading: {
+      editable: true,
+      level: '2',
+      text: 'Apéro',
+      visualLevel: '4',
+    },
+    index: '1',
+    ingredientsSelector: {
+      headingIngredients: 'Deine Zutaten',
+      headingIngredientsList: 'Zutaten suchen',
+      // ingredients,
+      input: {
+        closeButton: true,
+        dropdown: {
+          ...ingredientsAutocompleteInputDropdown,
         },
+        id: 'ingredients-input',
+        label: false,
+        placeholder: 'Zutaten durchsuchen',
       },
-    ],
-    radios: true,
+      pillsPlaceholder: 'Füge 2 bis 5 Zutaten hinzu.',
+    },
   },
 };
